747ae3bb76c9f682154f5ee3ef9de711903da6af
R7558 <rupsanaworld1004@gmail.co>
2023-05-30 13:29:06 +0600

    added header

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Article from '../Article/Article';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <Article></Article>
         </div>
     );
