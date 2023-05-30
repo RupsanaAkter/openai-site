da62881beafc27e85adc6cf8af2957326202a716
R7558 <rupsanaworld1004@gmail.co>
2023-05-30 15:20:05 +0600

    added features

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Article from '../Article/Article';
 import Header from '../Header/Header';
 import Brand from '../Brand/Brand';
 import Whatgpt from '../Whatgpt3/Whatgpt';
+import Features from '../Features/Features';
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
             <Brand></Brand>
            </div>
            <Whatgpt></Whatgpt>
+           <Features></Features>
             <Article></Article>
         </div>
     );
