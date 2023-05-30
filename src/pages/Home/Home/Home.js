c36c50eab0493c73d59cee0d5056840affa9da06
R7558 <rupsanaworld1004@gmail.co>
2023-05-30 13:56:17 +0600

    added brand

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Article from '../Article/Article';
 import Header from '../Header/Header';
+import Brand from '../Brand/Brand';
 
 const Home = () => {
     return (
-        <div>
-            <Header></Header>
+        <div className="App">
+           <div className="gradient__bg">
+           <Header></Header>
+            <Brand></Brand>
+           </div>
             <Article></Article>
         </div>
     );
